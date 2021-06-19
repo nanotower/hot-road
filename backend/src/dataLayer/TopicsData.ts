9e4689322ffee2c69ae9603ed4d30278c39e08da
Fer <fernandodelatorre4@gmail.com>
2021-06-20 01:03:21 +0200

    wip

--- backend/src/dataLayer/TopicsData.ts
+++ backend/src/dataLayer/TopicsData.ts
@@ -3,45 +3,49 @@
 // import { DocumentClient } from 'aws-sdk/clients/dynamodb'
 
 // const XAWS = AWSXRay.captureAWS(AWS)
-import DbClient from './DbClient'
+import DbClient from './DbClient';
 
-import { Topic } from '../interfaces/Topic'
+import { Topic } from '../interfaces/Topic';
 
-import { createLogger } from '../utils/logger'
-const logger = createLogger('TopicsData')
+import { createLogger } from '../utils/logger';
+const logger = createLogger('TopicsData');
 
 export class TopicsData extends DbClient {
   constructor(
-    private readonly topicsTable = process.env.TOPICS_TABLE,
-    // private readonly index = process.env.INDEX_NAME,
-    // private readonly bucketName = process.env.IMAGES_S3_BUCKET
-  ) {
-    super()
+    private readonly topicsTable = process.env.TOPICS_TABLE
+  ) // private readonly index = process.env.INDEX_NAME,
+  // private readonly bucketName = process.env.IMAGES_S3_BUCKET
+  {
+    super();
   }
 
   async createNewTopic(topic: Topic): Promise<Topic> {
-    logger.info('creating topic', {topic})
+    logger.info('creating topic', { topic });
     await this.docClient
-    .put({
-      TableName: this.topicsTable,
-      Item: {
-        ...topic
-      }
-    })
-    .promise()
+      .put({
+        TableName: this.topicsTable,
+        Item: {
+          ...topic,
+        },
+      })
+      .promise();
 
-    return topic
+    return topic;
   }
 
   async deleteTopic(topicId: string) {
-    logger.info('delete topic', {topicId})
+    logger.info('delete topic', { topicId });
     await this.docClient
-    .delete({
-      TableName: this.topicsTable,
-      Key: {
-        topicId
-      }
-    }).promise();
+      .delete(
+        {
+          TableName: this.topicsTable,
+          Key: {
+            topicId,
+          },
+        },
+        (err, data) => logger.info('res', { err, data })
+      )
+      .promise();
   }
 
   // async registerUser(user: UserItem): Promise<UserItem> {
@@ -54,89 +58,89 @@ export class TopicsData extends DbClient {
 
   //   return user as UserItem
   // }
-  
-//   export async function createGroup(
-//     createGroupRequest: CreateGroupRequest,
-//     jwtToken: string
-//   ): Promise<Group> {
-  
-//     const itemId = uuid.v4()
-//     const userId = getUserId(jwtToken)
-  
-//     return await groupAccess.createGroup({
-//       id: itemId,
-//       userId: userId,
-//       name: createGroupRequest.name,
-//       description: createGroupRequest.description,
-//       timestamp: new Date().toISOString()
-//     })
-//   }
-//   async getAllTodo(userId: string): Promise<TodoItem[]> {
-//     logger.info('getAllTodo', {userId})
-//     const todosList = await this.docClient
-//       .query({
-//         TableName: this.todoTable,
-//         IndexName: this.index,
-//         KeyConditionExpression: 'userId = :userId',
-//         ExpressionAttributeValues: {
-//           ':userId': userId
-//         }
-//       })
-//       .promise()
 
-//     const items = todosList.Items
-//     return items as TodoItem[]
-//   }
+  //   export async function createGroup(
+  //     createGroupRequest: CreateGroupRequest,
+  //     jwtToken: string
+  //   ): Promise<Group> {
 
-//   async createTodo(todo: TodoItem): Promise<TodoItem> {
-//     logger.info('create todo', {todo})
-//     const attachmentUrl = `https://${this.bucketName}.s3.amazonaws.com/${todo.todoId}`
-//     await this.docClient
-//       .put({
-//         TableName: this.todoTable,
-//         Item: {...todo, attachmentUrl }
-//       })
-//       .promise()
+  //     const itemId = uuid.v4()
+  //     const userId = getUserId(jwtToken)
 
-//     return todo
-//   }
+  //     return await groupAccess.createGroup({
+  //       id: itemId,
+  //       userId: userId,
+  //       name: createGroupRequest.name,
+  //       description: createGroupRequest.description,
+  //       timestamp: new Date().toISOString()
+  //     })
+  //   }
+  //   async getAllTodo(userId: string): Promise<TodoItem[]> {
+  //     logger.info('getAllTodo', {userId})
+  //     const todosList = await this.docClient
+  //       .query({
+  //         TableName: this.todoTable,
+  //         IndexName: this.index,
+  //         KeyConditionExpression: 'userId = :userId',
+  //         ExpressionAttributeValues: {
+  //           ':userId': userId
+  //         }
+  //       })
+  //       .promise()
 
-//   async deleteTodo(todo: TodoItem): Promise<{}> {
-//     logger.info('deleteTodo', {todo})
-//     await this.docClient
-//     .delete({
-//       TableName: this.todoTable,
-//       Key: {
-//         todoId: todo.todoId,
-//         userId: todo.userId
-//       }
-//     }).promise();
-//     return {}
-//   }
+  //     const items = todosList.Items
+  //     return items as TodoItem[]
+  //   }
 
-//   async updateTodo(todoUpdate: TodoUpdate, userId: string, todoId: string): Promise <{}>{
-//     logger.info('updateTodo', {todoUpdate, userId, todoId})
-//     await this.docClient.update({
-//       TableName: this.todoTable,
-//       Key: {
-//         todoId,
-//         userId
-//       },
-//       UpdateExpression: 'set #name = :n, #dueDate = :due, #done = :d',
-//       ExpressionAttributeValues: {
-//         ':n': todoUpdate.name,
-//         ':due': todoUpdate.dueDate,
-//         ':d': todoUpdate.done
-//       },
-//       ExpressionAttributeNames: {
-//         '#name': 'name',
-//         '#dueDate': 'dueDate',
-//         '#done': 'done'
-//       }
-//     }).promise();
+  //   async createTodo(todo: TodoItem): Promise<TodoItem> {
+  //     logger.info('create todo', {todo})
+  //     const attachmentUrl = `https://${this.bucketName}.s3.amazonaws.com/${todo.todoId}`
+  //     await this.docClient
+  //       .put({
+  //         TableName: this.todoTable,
+  //         Item: {...todo, attachmentUrl }
+  //       })
+  //       .promise()
 
-//     return {}
-//   }
+  //     return todo
+  //   }
+
+  //   async deleteTodo(todo: TodoItem): Promise<{}> {
+  //     logger.info('deleteTodo', {todo})
+  //     await this.docClient
+  //     .delete({
+  //       TableName: this.todoTable,
+  //       Key: {
+  //         todoId: todo.todoId,
+  //         userId: todo.userId
+  //       }
+  //     }).promise();
+  //     return {}
+  //   }
+
+  //   async updateTodo(todoUpdate: TodoUpdate, userId: string, todoId: string): Promise <{}>{
+  //     logger.info('updateTodo', {todoUpdate, userId, todoId})
+  //     await this.docClient.update({
+  //       TableName: this.todoTable,
+  //       Key: {
+  //         todoId,
+  //         userId
+  //       },
+  //       UpdateExpression: 'set #name = :n, #dueDate = :due, #done = :d',
+  //       ExpressionAttributeValues: {
+  //         ':n': todoUpdate.name,
+  //         ':due': todoUpdate.dueDate,
+  //         ':d': todoUpdate.done
+  //       },
+  //       ExpressionAttributeNames: {
+  //         '#name': 'name',
+  //         '#dueDate': 'dueDate',
+  //         '#done': 'done'
+  //       }
+  //     }).promise();
+
+  //     return {}
+  //   }
 }
 
 // function createDynamoDBClient() {
