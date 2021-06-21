9ad75972f7509a06953a098da39da77bad58363e
Fer <fernandodelatorre4@gmail.com>
2021-06-21 08:42:56 +0200

    add generate url

--- backend/src/businessLogic/Users.ts
+++ backend/src/businessLogic/Users.ts
@@ -6,6 +6,9 @@ import { NewUserRequest } from '../interfaces/NewUserRequest'
 import { createLogger } from '../utils/logger'
 const logger = createLogger('UsersLogic')
 
+import {S3Files} from '../dataLayer/S3Files'
+const s3Files = new S3Files()
+
 export const getUser = async (userId: string) => {
   const user = await usersData.getUser(userId)
   logger.info('Get user', {userId, user})
@@ -35,4 +38,12 @@ export const adjustUsersTopic = async (userId: string, operation: string) => {
   logger.info('Event', {userId})
   const updatedUser = await usersData.adjustUsersTopic(userId, operation)
   return updatedUser
+}
+
+export async function generateUrl(userId: string): Promise<string> {
+  logger.info('generate URL ', {userId})
+  const urlExpires = process.env.URL_EXPIRES
+
+  const urlSigned = s3Files.generateUploadURL(userId, urlExpires)
+  return urlSigned
 }
---
+++ backend/src/lambda/http/generateUploadUrl.ts
@@ -0,0 +1,23 @@
+import 'source-map-support/register'
+
+import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
+import { generateUrl } from '../../businessLogic/Users'
+import { createLogger } from '../../utils/logger'
+const logger = createLogger('GenerateURl')
+
+export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  const todoId = event.pathParameters.todoId
+  logger.info('Event generateURL', {todoId})
+  
+  const signedUrl = await generateUrl(todoId)
+  return {
+    statusCode: 202,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true
+    },
+    body: JSON.stringify({
+      uploadUrl: signedUrl
+    })
+  };
+}
